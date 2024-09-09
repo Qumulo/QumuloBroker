632583ea479d8196b0e58e64e535ed9f818a8da1
BeratUlualan <bulualan@qumulo.com>
2024-09-09 11:39:54 +0000

    Updated for branch 7.2.0

--- api/app/main.py
+++ api/app/main.py
@@ -43,7 +43,7 @@ from routers.objects import objects
 
 progname = "Qumulo RestAPIs for Varonis"
 progdesc = "Qumulo RestAPIs for Varonis"
-progvers = "7.2.1"
+progvers = "7.2.0"
 
 def parse_args():
     """Parse command-line arguments."""
